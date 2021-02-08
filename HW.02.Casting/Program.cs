be3546fa284e3ca181af1cc5bb1e7ad6d6aeda7c
ALEXANDER_MOSIN <sasha.mosin89@gmail.com>
2021-02-09 01:19:56 +0300

    HW02: added 3 homework projects

---
+++ HW.02.Casting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace HW._02.Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {    // Неявное преобразование типов
+                sbyte a = 15;
+                short b = a;
+                int c = b;
+                long d = c;
+            }
+            {   // Явное преобразовани типов       
+                decimal a = 15;
+                uint b = (uint)a;
+                ushort c = (ushort)b;
+                sbyte d = (sbyte)c;
+            }
+            {   // Упаковка и распаковка
+                int a = 5;
+                decimal b = 100;
+                string text = "Распаковка и упаковка";
+                object a1 = a; //упаковка
+                object a2 = text; //упаковка
+                object a3 = b; //упаковка
+                int b1 = (int)a1;//распаковка
+                string b2 = text;//распаковка
+                decimal b3 = (decimal)a3;//распаковка
+                System.Console.WriteLine(b1);
+                System.Console.WriteLine(b2);
+                System.Console.WriteLine(b3);
+                Console.ReadKey();
+                //сборка работает корректно, т.к. в результате образовался .exe и .dll 
+            }
+        }
+    }
+}
